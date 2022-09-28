ba5fba0576830a83959b3e09c232c4125e800d4e
Shourya Naik <sjnaik02@gmail.com>
2022-09-28 10:36:01 +0530

    now displays username and timestamp on messages

--- src/components/ChatRoom.jsx
+++ src/components/ChatRoom.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 import useChat from "../hooks/useChat";
 import { useState } from "react";
 
@@ -6,46 +6,59 @@ function ChatRoom() {
   const { roomid } = useParams();
   const { messages, sendMessage } = useChat(roomid);
   const [newMessage, setNewMessage] = useState("");
+  //get username passed through state
+  const { state } = useLocation();
+  const username = state.user;
 
   const handleChange = (e) => {
     setNewMessage(e.target.value);
   };
 
-  //implement enter to send
+  const handleSend = () => {
+    sendMessage(newMessage, username);
+    setNewMessage("");
+  };
+
   const handleKeyDown = (e) => {
     if (e.key === "Enter") {
-      sendMessage(newMessage);
-      setNewMessage("");
+      handleSend();
     }
   };
   return (
-    <div>
-      <h1 className="text-3xl text-white">Murmur</h1>
+    <div className="w-screen h-screen">
+      <h1 className="text-3xl text-black text-center">Murmur</h1>
       <div className="flex flex-col items-center justify-center">
-        <div className="w-80 h-96 bg-gray-900 rounded-lg overflow-y-scroll">
+        <div className="w-screen lg:w-96 mx-4  h-[80vh] bg-gray-900 rounded-lg overflow-y-scroll">
           {messages.map((message) => (
             <div className="p-4">
+              <p className="text-green-300">
+                {message.sender}{" "}
+                <span className="text-gray-300 text-xs">
+                  {message.timestamp}
+                </span>
+              </p>
               <p className="text-white">{message.body}</p>
             </div>
           ))}
         </div>
-        <input
-          type="text"
-          placeholder="Message"
-          className="w-80 p-4  h-10 rounded-lg border-2 border-gray-900"
-          value={newMessage}
-          onChange={handleChange}
-          onKeyDown={handleKeyDown}
-        />
-        <button
-          className="bg-blue-500 text-white p-2 rounded-lg"
-          onClick={() => {
-            sendMessage(newMessage);
-            setNewMessage("");
-          }}
-        >
-          Send
-        </button>
+        <div className="w-screen lg:w-96 h-12 flex">
+          <input
+            type="text"
+            placeholder="Message"
+            className="w-full p-4  h-full rounded-lg border-2 border-gray-900"
+            value={newMessage}
+            onChange={handleChange}
+            onKeyDown={handleKeyDown}
+          />
+          <button
+            className="bg-blue-500 text-white p-2 w-auto h-full rounded-lg"
+            onClick={() => {
+              handleSend();
+            }}
+          >
+            Send
+          </button>
+        </div>
       </div>
     </div>
   );
