dc0c39e44de5e3cf5d2bd55a6b5df3b94b60c4a2
Shourya Naik <sjnaik02@gmail.com>
2022-09-28 10:42:43 +0530

    styling improvements

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,25 +13,25 @@ function Home() {
   };
 
   return (
-    <div>
+    <div className="w-screen h-screen flex items-center justify-center flex-col">
       <h1 className="text-3xl text-center">Murmur</h1>
       <div className="flex flex-col items-center justify-center">
         <input
           type="text"
           placeholder="Room Name"
-          className="w-80 p-4  h-10 rounded-lg border-2 border-gray-900"
+          className="w-80 p-4 my-2  h-10 rounded-lg border-2 border-gray-900"
           value={roomName}
           onChange={(e) => setRoomName(e.target.value)}
         />
         <input
           type="text"
           placeholder="Username"
-          className="w-80 p-4  h-10 rounded-lg border-2 border-gray-900"
+          className="w-80 p-4  h-10 my-2 rounded-lg border-2 border-gray-900"
           value={userName}
           onChange={(e) => setUserName(e.target.value)}
         />
         <button
-          className="bg-blue-500 text-white p-2 rounded-lg"
+          className="bg-blue-500 my-2 text-white p-2 rounded-lg"
           onClick={handleClick}
         >
           Join Room
