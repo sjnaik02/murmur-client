49081e3ee4ffdd9de83a6ca48bdab4821537b362
Shourya Naik <sjnaik02@gmail.com>
2022-09-28 12:30:26 +0530

    stupid error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
-import Chatroom from "./components/Chatroom";
+import ChatRoom from "./components/ChatRoom";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/:roomid" element={<Chatroom />} />"
+        <Route path="/:roomid" element={<ChatRoom />} />"
       </Routes>
     </Router>
   );
